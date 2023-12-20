24fbbe6d64bd3c6e70b15a8a3da0eb1109d33063
G Nithyalakshmi <93335949+Nithya9404@users.noreply.github.com>
2023-12-20 18:47:01 +0530

    Update questions-part-b.component.ts

--- mark_entry/src/app/components/questions-part-b/questions-part-b.component.ts
+++ mark_entry/src/app/components/questions-part-b/questions-part-b.component.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { RegisterService } from 'src/app/services/register.service';
+
 @Component({
   selector: 'app-questions-part-b',
   templateUrl: './questions-part-b.component.html',
   styleUrls: ['./questions-part-b.component.css']
 })
-export class QuestionsPartBComponent {
+export class QuestionsPartBComponent implements OnInit {
   questionAnswers: { register: string, marks: number, q: number[] }[] = [];
 
-  constructor(private router: Router, private http: HttpClient) {
-    // Initialize the questionAnswers array with empty objects
-    for (let i = 0; i < 10; i++) {
-      this.questionAnswers.push({ register: `R${i + 1}`, marks: 0, q: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] });
-    }
+  constructor(private router: Router, private http: HttpClient, private registerService: RegisterService) {}
+
+  ngOnInit() {
+    this.registerService.getRegisterNumbers().subscribe(
+      (registerNumbers: string[]) => {
+        for (let i = 0; i < registerNumbers.length; i++) {
+          this.questionAnswers.push({ register: registerNumbers[i], marks: 0, q: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] });
+        }
+      },
+      (error) => {
+        console.error('Error fetching register numbers:', error);
+      }
+    );
   }
 
   checkValue(value: number) {
@@ -23,17 +33,13 @@ export class QuestionsPartBComponent {
   }
 
   sendtoDatabase() {
-    // Assuming you have collected the questionAnswers data and want to send it to the server
     const postData = this.questionAnswers.map((item) => {
       return { q: item.q };
     });
 
-    // Make an HTTP POST request to your Express.js server
     this.http.post('http://localhost:4000/api/insertData', postData).subscribe(
-      (response: any) => { // Use 'any' type for response since the server response format may vary
+      (response: any) => {
         console.log('Data inserted successfully', response);
-        
-        // Assuming you want to navigate to the next component after a successful response
         this.router.navigate(['/questions_part_B']);
       },
       (error) => {
