b888ad966e2bc494d094b96d5099faf1e928793e
G Nithyalakshmi <93335949+Nithya9404@users.noreply.github.com>
2023-12-20 18:44:45 +0530

    Update question_type.js

--- backend/question_type.js
+++ backend/question_type.js
@@ -3,31 +3,32 @@ const cors = require('cors');
 const pg = require('pg');
 const bodyparser = require('body-parser')
 
-const app=express();
-const port =3002;
+const app = express();
+const port = 3002;
 
 app.use(bodyparser.json())
 app.use(cors());
 
 const pool = new pg.Pool({
-    user:'postgres',
-    host:'localhost',
-    database:'mark_entry',
-    password:'nithya9404',
-    port:5432
+  user: 'postgres',
+  host: 'localhost',
+  database: 'mark_entry',
+  password: 'nithya9404',
+  port: 5432
 });
 
-app.get('/dropdown-data',(req,res) => {
-    pool.query('SELECT assessment_name FROM other_assessment_master;',(error,results) => {
-       if (error){
-        console.error('Error fetching the data: ',error);
-        res.status(500).json({error:'Internal server error'});
-       }
-       else{
-        res.json(results.rows);
-        console.log('Values',results.rows);
-       }
-    });
+let currentYear; // Declare a variable to store the currentYear globally
+
+app.get('/dropdown-data', (req, res) => {
+  pool.query('SELECT assessment_name FROM other_assessment_master;', (error, results) => {
+    if (error) {
+      console.error('Error fetching the data: ', error);
+      res.status(500).json({ error: 'Internal server error' });
+    } else {
+      res.json(results.rows);
+      console.log('Values', results.rows);
+    }
+  });
 });
 
 app.post('/authenticate', (req, res) => {
@@ -49,46 +50,58 @@ app.post('/authenticate', (req, res) => {
     }
   });
 });
+
 app.get('/getCourseCodes/:facultyId', (req, res) => {
-  const requestedFacultyId = req.params.facultyId;
+    const requestedFacultyId = req.params.facultyId;
+    const query = {
+      text: 'SELECT batch_no, faculty_id1, course_code, dept_code FROM course_allocation_faculty WHERE faculty_id1 = $1',
+      values: [requestedFacultyId],
+    };
+    pool.query(query, (err, result) => {
+      if (err) {
+        console.error('Error executing SQL query:', err);
+        res.status(500).json({ success: false, message: 'Database error' });
+      } else {
+        const courseCodes = result.rows.map((row) => row.course_code);
+        const batch = result.rows.map((row) => row.batch_no);
+        const facultyId = result.rows.map((row) => row.faculty_id1);
+        const deptcode = result.rows.map((row) => row.dept_code);
+  
+        // Set the currentYear based on your business logic
+        // Here, I'm assuming the first batch is the current year. Modify as per your requirements.
+        const currentYearIndex = 0;
+  
+        if (batch.length > currentYearIndex) {
+          currentYear = batch[currentYearIndex];
+        } else {
+          currentYear = null;
+        }
+  
+        res.json({ facultyId, batch, courseCodes, deptcode, currentYear });
+      }
+    });
+  });
+  
+
+app.get('/department/:deptcode', (req, res) => {
+  const requestedDeptcode = req.params.deptcode;
   const query = {
-    text: 'SELECT batch_no, faculty_id1, course_code,dept_code FROM course_allocation_faculty WHERE faculty_id1 = $1',
-    values: [requestedFacultyId],
+    text: 'SELECT dept_name FROM department_master WHERE dept_code=$1',
+    values: [requestedDeptcode]
   };
   pool.query(query, (err, result) => {
     if (err) {
       console.error('Error executing SQL query:', err);
-      res.status(500).json({ success: false, message: 'Database error' });
+      res.status(500).json({ success: false, message: 'Internal server error' });
     } else {
-      const courseCodes = result.rows.map((row) => row.course_code);
-      const batch = result.rows.map((row) => row.batch_no);
-      const facultyId = result.rows.map((row) => row.faculty_id1);
-      const deptcode = result.rows.map((row) => row.dept_code);
-      res.json({ facultyId, batch, courseCodes,deptcode });
+      const department = result.rows[0] ? result.rows[0].dept_name : null;
+      res.json({ department });
     }
   });
 });
 
-app.get('/department/:deptcode',(req,res) => {
-   const requestedDeptcode = req.params.deptcode;
-   const query ={
-    text: 'SELECT dept_name FROM department_master WHERE dept_code=$1',
-    values:[requestedDeptcode]
-   };
-   pool.query(query,(err,result)=>{
-      if(err){
-        console.error('Error executing SQL query:',err);
-        res.status(500).json({success: false,message:'Internal server error'});
-      }
-      else {
-      const department = result.rows[0] ? result.rows[0].dept_name : null;
-      res.json({ department });
-      }
-   });
-});
-
 app.get('/course_title/:courseCodes', (req, res) => {
-  const requestCourseCodes = req.params.courseCodes.split(','); 
+  const requestCourseCodes = req.params.courseCodes.split(',');
   const query = {
     text: 'SELECT course_title FROM course_master WHERE course_code = ANY($1)',
     values: [requestCourseCodes],
@@ -104,6 +117,24 @@ app.get('/course_title/:courseCodes', (req, res) => {
   });
 });
 
-app.listen(port,()=>{
-    console.log(`Server is running on ${port}`)
+app.get('/api/registerNumbers', async (req, res) => {
+  // Using the globally stored currentYear
+  if (!currentYear) {
+    return res.status(400).send('Current year not available');
+  }
+
+  const query = 'SELECT register_number FROM register_numbers WHERE year = $1';
+
+  try {
+    const result = await pool.query(query, [currentYear]);
+    const registerNumbers = result.rows.map((row) => row.register_number);
+    res.json(registerNumbers);
+  } catch (error) {
+    console.error('Error querying database:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
 });
