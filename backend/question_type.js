dc57d1ecd02b5e5cc3693f2de266902666fdb9d8
G Nithyalakshmi <93335949+Nithya9404@users.noreply.github.com>
2024-01-11 17:12:32 +0530

    Update question_type.js

--- backend/question_type.js
+++ backend/question_type.js
@@ -17,7 +17,7 @@ const pool = new pg.Pool({
   port: 5432
 });
 
-let currentYear; // Declare a variable to store the currentYear globally
+let currentYear; 
 
 app.get('/dropdown-data', (req, res) => {
   pool.query('SELECT assessment_name FROM other_assessment_master;', (error, results) => {
@@ -52,36 +52,33 @@ app.post('/authenticate', (req, res) => {
 });
 
 app.get('/getCourseCodes/:facultyId', (req, res) => {
-    const requestedFacultyId = req.params.facultyId;
-    const query = {
-      text: 'SELECT batch_no, faculty_id1, course_code, dept_code FROM course_allocation_faculty WHERE faculty_id1 = $1',
-      values: [requestedFacultyId],
-    };
-    pool.query(query, (err, result) => {
-      if (err) {
-        console.error('Error executing SQL query:', err);
-        res.status(500).json({ success: false, message: 'Database error' });
+  const requestedFacultyId = req.params.facultyId;
+  const query = {
+    text: 'SELECT batch_no, faculty_id1, course_code, dept_code, degree_code,regulation_no FROM course_allocation_faculty WHERE faculty_id1 = $1',
+    values: [requestedFacultyId],
+  };
+  pool.query(query, (err, result) => {
+    if (err) {
+      console.error('Error executing SQL query:', err);
+      res.status(500).json({ success: false, message: 'Database error' });
+    } else {
+      const courseCodes = result.rows.map((row) => row.course_code);
+      const batch = result.rows.map((row) => row.batch_no);
+      const facultyId = result.rows.map((row) => row.faculty_id1);
+      const deptcode = result.rows.map((row) => row.dept_code);
+      const degreeCode = result.rows.map((row) => row.degree_code);
+      const regulation = result.rows.map((row) => row.regulation_no);
+      const currentYearIndex = 0;
+      if (batch.length > currentYearIndex) {
+        currentYear = batch[currentYearIndex];
       } else {
-        const courseCodes = result.rows.map((row) => row.course_code);
-        const batch = result.rows.map((row) => row.batch_no);
-        const facultyId = result.rows.map((row) => row.faculty_id1);
-        const deptcode = result.rows.map((row) => row.dept_code);
-  
-        // Set the currentYear based on your business logic
-        // Here, I'm assuming the first batch is the current year. Modify as per your requirements.
-        const currentYearIndex = 0;
-  
-        if (batch.length > currentYearIndex) {
-          currentYear = batch[currentYearIndex];
-        } else {
-          currentYear = null;
-        }
-  
-        res.json({ facultyId, batch, courseCodes, deptcode, currentYear });
+        currentYear = null;
       }
-    });
+
+      res.json({ facultyId, batch, courseCodes, deptcode, degreeCode, currentYear, regulation});
+    }
   });
-  
+});
 
 app.get('/department/:deptcode', (req, res) => {
   const requestedDeptcode = req.params.deptcode;
@@ -118,7 +115,6 @@ app.get('/course_title/:courseCodes', (req, res) => {
 });
 
 app.get('/api/registerNumbers', async (req, res) => {
-  // Using the globally stored currentYear
   if (!currentYear) {
     return res.status(400).send('Current year not available');
   }
@@ -134,6 +130,61 @@ app.get('/api/registerNumbers', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
+
+app.post('/api/insert', async (req, res) => {
+  try {
+    // Insert data into the 'partb' table
+    const data = req.body;
+    const formattedData = data.map((item) => item.q);
+
+    const query = format(
+      'INSERT INTO question_pattern_1 (q1, q2, q3, q4, q5, q6, q7, q8, q9, q10) VALUES %L',
+      formattedData
+    );
+
+    await pool.query(query);
+    console.log('Data inserted successfully into partb table');
+    res.status(200).json({ message: 'Data inserted successfully' });
+  } catch (error) {
+    console.error('Error inserting data into partb table:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.post('/api/insertData', async (req, res) => {
+  try {
+    const registerNumbersResponse = await fetch('http://localhost:3002/api/registerNumbers');
+    const registerNumbers = await registerNumbersResponse.json();
+    for (const registerNumber of registerNumbers) {
+      const data = req.body;
+      const { column1, column2, column3, column4,column6,column7 } = data;
+      console.log('Data: ', data);
+      const query1 = {
+        text: 'INSERT INTO question_pattern_1 (batch_no, semester, course_code, degree_code, reg_no,dept_code,regulation_no) VALUES ($1, $2, $3, $4, $5,$6,$7)',
+        values: [column1, column2, column3, column4[0], registerNumber,column6[0],column7[0]],
+      };
+      const query2 = {
+        text: 'INSERT INTO iat_marks (batch_no, semester, course_code, degree_code, reg_no,dept_code,regulation_no) VALUES ($1, $2, $3, $4, $5,$6,$7)',
+        values: [column1, column2, column3, column4[0], registerNumber,column6[0],column7[0]],
+      };
+      const query3 = {
+        text: 'INSERT INTO co_level_marks (batch_no, semester, course_code, degree_code, reg_no,dept_code,regulation_no) VALUES ($1, $2, $3, $4, $5,$6,$7)',
+        values: [column1, column2, column3, column4[0], registerNumber,column6[0],column7[0]],
+      };
+      await Promise.all([
+        pool.query(query1),
+        pool.query(query2),
+        pool.query(query3),
+      ]);
+    }
+    console.log('Data inserted successfully into three tables');
+    res.status(200).json({ message: 'Data inserted successfully' });
+  } catch (error) {
+    console.error('Error inserting data into tables:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
