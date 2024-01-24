09b0bf875231fbda57fc9b7cd585d24d51e33e2a
G Nithyalakshmi <93335949+Nithya9404@users.noreply.github.com>
2024-01-24 21:05:32 +0530

    Update question-type.component.ts

--- mark_entry/src/app/components/question-type/question-type.component.ts
+++ mark_entry/src/app/components/question-type/question-type.component.ts
@@ -1,21 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
+import { SharedDataService } from 'src/app/services/shareddata.service';
 
 @Component({
   selector: 'app-question-type',
   templateUrl: './question-type.component.html',
   styleUrls: ['./question-type.component.css']
 })
-export class QuestionTypeComponent implements OnInit{
-  dropdownData: any[]=[];
+export class QuestionTypeComponent implements OnInit {
+  dropdownData: any[] = [];
+  selectedAssessment: any = '';
 
-  constructor(private dataService:DataService){}
+  constructor(
+    private dataService: DataService,
+    private sharedDataService: SharedDataService
+  ) {}
 
   ngOnInit() {
-    this.dataService.getDropDowndata().subscribe((data: any)=>{
-        this.dropdownData = data;
-        console.log('Dropdown Data:',this.dropdownData);
+    this.dataService.getDropDowndata().subscribe((data: any) => {
+      this.dropdownData = data;
+      console.log('Dropdown Data:', this.dropdownData);
     });
   }
 
+  onAssessmentSelected() {
+    console.log("Selected assessment (before): ", this.selectedAssessment);
+    this.sharedDataService.setSelectedAssessment(this.selectedAssessment);
+    console.log("Selected assessment (after): ", this.selectedAssessment);
+  }
+  
 }
